7181bd66ff55e57a5fd6acbbf8e89cd9492d3c31
Jason M <jbird206@gmail.com>
2020-01-15 21:00:33 -0800

    Add files via upload

---
+++ survey-forms/js/scripts.js
@@ -0,0 +1,14 @@
+// Business(back-end) logic ///////////////////////////////
+function myFunction() {
+var flavor = document.getElementById("theRadio").value;
+var beverage = $("#beverage").val();
+
+
+
+
+
+// user interface (front-end) logic:
+//gathers user input values,parses them into intergers and calling our add() function when user submits the form with the button.
+
+  document.getElementById("output1").innerHTML = flavor;
+  }
